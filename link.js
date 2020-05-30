36e3456d9437cf35c311adf87287f3d13490b680
Melvin <melvincarvalho@gmail.com>
2020-05-31 00:37:57 +0200

    allow data: uris

--- link.js
+++ link.js
@@ -1,2 +1,4 @@
 import 'https://unpkg.com/dataisland?module'
-document.location.href = di.data[0].uri
+var _
+( _ = new URL(di.data[0].uri)).protocol == 'data:' ? document.body.insertAdjacentHTML('beforeend',`<i
+mg src="${_}">`) : document.location.href = _;
